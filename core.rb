a9d5546d808856e39bb82e52dc11ada5d27db99d
dokinho <doks7081@live.com>
2022-05-02 16:50:19 +0200

    Working main menu

--- core.rb
+++ core.rb
@@ -2,16 +2,32 @@
 require "rubygems"
 require "bundler/Setup"
 
+require "tty-prompt"
+require "tty-reader"
+
 # Requires all files from "lib" folder
 Dir[File.join(__dir__, 'lib', '*.rb')].each { |file| require file }
+
+# -----TEST-----
+@igrac = Player.new
+@zloco = Enemy.new(10, 1, 100)
+@mapa = Map.new("Mapa", 20, 20)
+
+@mapa.add_object(@igrac, 0, 1)
+@mapa.add_object(@zloco, 5, 5)
+
+@prompt = TTY::Prompt.new
+@reader = TTY::Reader.new
 
 class Game
 
 end
 
 def game_loop
+  State.init(@prompt, @reader, @igrac, @mapa)
+  choice = "Menu"
   loop do
-    State.menu
+    choice = eval("State.#{choice.downcase}")
   end
 end
 
--- lib/State.rb
+++ lib/State.rb
@@ -1,40 +1,17 @@
-require "tty-prompt"
-require "tty-reader"
-
 module State
-  # -----TEST-----
-  @igrac = Player.new
-  @zloco = Enemy.new(10, 1, 100)
-  @mapa = Map.new("Mapa", 20, 20)
+  # @prompt, @reader, @player, @map = nil, nil, nil, nil
 
-  @mapa.add_object(@igrac, 0, 1)
-  @mapa.add_object(@zloco, 5, 5)
-
-  @prompt = TTY::Prompt.new
-  @reader = TTY::Reader.new
+  def self.init(prompt, reader, player, map)
+    @prompt = prompt
+    @reader = reader
+    @player = player
+    @map = map
+  end
 
   def self.menu
     system "cls"
     selection = %w(Map Character Inventory Quests Quit)
-    choice = @prompt.select("-----MAIN MENU-----", selection)
-
-    case choice
-    when "Map"
-      self.map
-    when "Character"
-      self.character
-    when "Inventory"
-      self.inventory
-    when "Quests"
-      self.quests
-    when "Quit"
-      system "cls"
-      puts "Thanks for playing!"
-      sleep 1
-      exit
-    else
-      puts "Stop cheating!"
-    end
+    @prompt.select("-----MAIN MENU-----", selection)
   end
 
   def self.map
@@ -43,56 +20,65 @@ module State
     puts "# -> Player"
     puts "@ -> Enemy"
     puts "$ -> Shop"
-    @mapa.render
-    choice = @prompt.select("Choose an action:", %w(Walk Back) )
-    case choice
-    
-    when "Walk"
-      loop do
-        system "cls"
-        @mapa.render
-        input = @reader.read_keypress
-        case input
-        when "w"
-          @mapa.move_object(@igrac, @igrac.pos_x - 1, @igrac.pos_y)
-        when "s"
-          @mapa.move_object(@igrac, @igrac.pos_x + 1, @igrac.pos_y)
-        when "a"
-          @mapa.move_object(@igrac, @igrac.pos_x, @igrac.pos_y - 1)
-        when "d"
-          @mapa.move_object(@igrac, @igrac.pos_x, @igrac.pos_y + 1)
-        when "q"
-          break
-        end
+    @map.render
+    @prompt.select("Choose an action:", %w(Walk Menu) )
+  end
+
+  def self.walk
+    loop do
+      system "cls"
+      @map.render
+      input = @reader.read_keypress
+      case input
+      when "w"
+        @map.move_object(@player, @player.pos_x - 1, @player.pos_y)
+      when "s"
+        @map.move_object(@player, @player.pos_x + 1, @player.pos_y)
+      when "a"
+        @map.move_object(@player, @player.pos_x, @player.pos_y - 1)
+      when "d"
+        @map.move_object(@player, @player.pos_x, @player.pos_y + 1)
+      when "q"
+        break
       end
     end
-
+    "Map"
   end
 
   def self.character
     system "cls"
     puts "-----Player-----"
     puts "|Name||Level||Health||Damage||Armor||Weapon|"
-    puts "#{@igrac.name}   #{@igrac.level}   #{@igrac.health}/#{@igrac.max_health}    #{@igrac.damage}       #{@igrac.armor}    #{@igrac.equipped_weapon}"
+    puts "#{@player.name}   #{@player.level}   #{@player.health}/#{@player.max_health}    #{@player.damage}       #{@player.armor}    #{@player.equipped_weapon}"
     puts
     puts "Press any key to go back"
     @reader.read_keypress
+    "Menu"
   end
 
   def self.inventory
     system "cls"
-    @igrac.inventory.each { |item| puts item }
+    @player.inventory.each { |item| puts item }
     puts
     puts "Press any key to go back"
     @reader.read_keypress
+    "Menu"
   end
 
   def self.quests
     system "cls"
-    @igrac.quests.each { |quest| puts quest }
+    @player.quests.each { |quest| puts quest }
     puts
     puts "Press any key to go back"
     @reader.read_keypress
+    "Menu"
+  end
+
+  def self.quit
+    system "cls"
+    puts "Thanks for playing!"
+    sleep 1
+    exit
   end
 
   def self.combat
