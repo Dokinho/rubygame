4a2a7f0085d29c672d418d5dd17a53e2c87204c7
dokinho <doks7081@live.com>
2022-05-03 19:20:17 +0200

    Added ability and ability spec

--- lib/Ability.rb
+++ lib/Ability.rb
@@ -1,3 +1,17 @@
 class Ability
+  attr_reader :name, :description, :type, :attribute
+  attr_accessor :amount
 
+  def initialize(name, description, type, amount, attribute)
+    @name = name
+    @description = description
+    @type = type
+    @amount = amount
+    @attribute = attribute
+  end
+
+  def activate(target)
+    str = "target.#{@attribute} #{@type}= #{@amount}"
+    eval(str)
+  end
 end
---
+++ spec/ability_spec.rb
@@ -0,0 +1,23 @@
+require_relative "../lib/Ability"
+require_relative "../lib/Enemy"
+
+RSpec.describe Ability do
+  subject { Ability.new("Attack", "Basic attack", "-", 10, "health") }
+
+  let(:bad_guy) { instance_double(Enemy, "Bad Guy", health: 100) }
+
+  context "attributes" do
+    it { is_expected.to have_attributes(name: "Attack") }
+    it { is_expected.to have_attributes(description: "Basic attack") }
+    it { is_expected.to have_attributes(type: "-") }
+    it { is_expected.to have_attributes(amount: 10) }
+    it { is_expected.to have_attributes(attribute: "health") }
+  end
+
+  context "#activate method" do
+    it "changes the target's attributes" do
+      expect(bad_guy).to receive(:health=).with(90).exactly(1).times
+      subject.activate(bad_guy)
+    end
+  end
+end
